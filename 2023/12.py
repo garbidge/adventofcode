fb1d4dc26aa71277f247ec4d8a69c7b442cc298c
sleeplessghost <sleeplessghost@live.com>
2023-12-12 23:51:12 +1030

    tidy slightly

--- 2023/12.py
+++ 2023/12.py
@@ -8,25 +8,24 @@ def parse(input):
 def part_a(data):
     return sum(cache_perms(line, counts) for line,counts in data)
 
-@cache
-def cache_perms(line, counts, index=0):
-    return sum(perms(line, counts, index))
+def part_b(data):
+    data = [('?'.join(line for _ in range(5)), counts*5) for line,counts in data]
+    return part_a(data)
 
-def perms(line, counts, index=0):
-    if index >= len(counts):
-        yield '#' not in line
+@cache
+def cache_perms(line, counts):
+    return sum(perms(line, counts))
+
+def perms(line, counts):
+    if len(counts) == 0: yield '#' not in line
     else:
-        count = counts[index]
+        count = counts[0]
         possibles = ((line[:i], line[i:i+count], line[i+count:]) for i in range(len(line) - count + 1))
         for prefix, removed, suffix in possibles:
             if '#' in prefix: break
             if '.' in removed: continue
             if (len(suffix) == 0 or suffix[0] != '#') and (len(prefix) == 0 or prefix[-1] != '#'):
-                yield cache_perms(suffix[1:], counts, index + 1)
-
-def part_b(data):
-    data = [('?'.join(line for _ in range(5)), counts*5) for line,counts in data]
-    return part_a(data)
+                yield cache_perms(suffix[1:], counts[1:])
 
 puzzle = Puzzle(2023, 12)
 data = parse(puzzle.input_data)
