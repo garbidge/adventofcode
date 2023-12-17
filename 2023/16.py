33b51f25cdd633d6c215ad9b6d3b900c0ab1c563
sleeplessghost <sleeplessghost@live.com>
2023-12-18 05:42:54 +1030

    16 17

---
+++ 2023/16.py
@@ -0,0 +1,53 @@
+from collections import deque
+from aocd.models import Puzzle
+from utils import maxes, pgriddict, tuple_add
+
+directions = {'u': (0,-1), 'd': (0,1), 'l': (-1,0), 'r': (1, 0)}
+mutations = {
+    ('|', 'l'): ('u','d'),
+    ('|', 'r'): ('u','d'),
+    ('-', 'u'): ('l','r'),
+    ('-', 'd'): ('l','r'),
+    ('/', 'u'): 'r',
+    ('\\','d'): 'r',
+    ('/', 'd'): 'l',
+    ('\\','u'): 'l',
+    ('/', 'l'): 'd',
+    ('\\','r'): 'd',
+    ('/', 'r'): 'u',
+    ('\\','l'): 'u'
+}
+
+def parse(input):
+    return pgriddict(input, str)
+
+def part_a(data, start = (0,0), start_dir = 'r'):
+    q = deque(((start, d) for d in next_direction(data[start], start_dir)))
+    seen = set(((start, start_dir),))
+    while q:
+        coord, d = q.popleft()
+        next_coord = tuple_add(coord, directions[d])
+        if next_coord in data and (next_coord, d) not in seen:
+            seen.add((next_coord, d))
+            for next_d in next_direction(data[next_coord], d):
+                q.append((next_coord, next_d))
+    return len(set(coord for coord,d in seen))
+
+def next_direction(char, d):
+    if (char, d) in mutations:
+        yield from mutations[(char, d)]
+    else: yield d
+
+def part_b(data):
+    max_x, max_y = maxes(data)
+    return max(
+        *(part_a(data, (x,     0), 'd') for x in range(max_x + 1)),
+        *(part_a(data, (x, max_y), 'u') for x in range(max_x + 1)),
+        *(part_a(data, (0,     y), 'r') for y in range(max_y + 1)),
+        *(part_a(data, (max_x, y), 'l') for y in range(max_y + 1))
+    )
+
+puzzle = Puzzle(2023, 16)
+data = parse(puzzle.input_data)
+print("part A", part_a(data))
+print("part B", part_b(data))
---
+++ 2023/17.py
@@ -0,0 +1,75 @@
+from heapq import heappop, heappush
+from aocd.models import Puzzle
+from utils import maxes, pgriddict, tuple_add
+
+directions = ['l','u','r','d']
+dirmap = {'u': (0,-1), 'd': (0,1), 'l': (-1,0), 'r': (1, 0)}
+
+def parse(input):
+    return pgriddict(input, int, lambda v: int(v))
+
+def part_a(data):
+    h = []
+    heappush(h, (0, (0,0), 'r', 0))
+    max_x, max_y = maxes(data)
+    end = (max_x, max_y)
+    seen = set()
+    while h:
+        dist, point, direction, length = heappop(h)
+        if point == end:
+            return dist
+        else:
+            for next_state in next_nodes(point, direction, length):
+                nextp, nextd, nextl = next_state
+                if nextp in data and next_state not in seen:
+                    seen.add(next_state)
+                    heappush(h, (dist + data[nextp], nextp, nextd, nextl))
+
+def next_nodes(point, direction, length):
+    if length < 3:
+        nextp = tuple_add(point, dirmap[direction])
+        yield (nextp, direction, length + 1)
+    for d in rotate(direction):
+        nextp = tuple_add(point, dirmap[d])
+        yield (nextp, d, 1)
+
+def rotate(direction):
+    index = directions.index(direction)
+    return (directions[i % len(directions)] for i in (index-1, index+1))
+
+def part_b(data):
+    h = []
+    heappush(h, (sum(data[p] for p in ((1,0), (2,0), (3,0))), (3,0), 'r', 3))
+    heappush(h, (sum(data[p] for p in ((0,1), (0,2), (0,3))), (0,3), 'd', 3))
+    max_x, max_y = maxes(data)
+    end = (max_x, max_y)
+    seen = set()
+    while h:
+        dist, point, direction, length = heappop(h)
+        if point == end:
+            return dist
+        else:
+            for ndist, npoint, ndir, nlen in next_nodes_b(data, dist, point, direction, length):
+                next_state = (npoint, ndir, nlen)
+                if npoint in data and next_state not in seen:
+                    seen.add(next_state)
+                    heappush(h, (ndist, npoint, ndir, nlen))
+
+def next_nodes_b(data, dist, point, direction, length):
+    if length < 10:
+        nextp = tuple_add(point, dirmap[direction])
+        if nextp in data:
+            yield (dist + data[nextp], nextp, direction, length + 1)
+    for d in rotate(direction):
+        nextp = point
+        ndist = dist
+        for _ in range(4):
+            nextp = tuple_add(nextp, dirmap[d])
+            if nextp in data: ndist += data[nextp]
+        if nextp in data:
+            yield (ndist, nextp, d, 4)
+
+puzzle = Puzzle(2023, 17)
+data = parse(puzzle.input_data)
+print("part A", part_a(data))
+print("part B", part_b(data))
