f66fdf2361848175a76ab59d6766bd7ccf747eae
sleeplessghost <sleeplessghost@live.com>
2023-12-18 17:11:49 +1030

    comment mathshit

--- 2023/18.py
+++ 2023/18.py
@@ -17,7 +17,9 @@ def part_a(data):
     for i in range(len(points) - 1):
         (x1,y1), (x2,y2) = points[i], points[i+1]
         perimeter += abs(x1-x2) + abs(y1-y2)
+        # shoelace formula
         volume += ((x1 * y2) - (x2 * y1)) / 2
+    # pick's theorem
     return int(volume + (perimeter / 2) + 1)
 
 def part_b(data):
