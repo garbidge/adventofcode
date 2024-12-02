fa491a05011210cdb503a9f91bd609da7f5ba698
garbidge <sleeplessghost@live.com>
2024-12-02 15:15:05 +1030

    tidying

--- 2024/01.py
+++ 2024/01.py
@@ -6,16 +6,14 @@ def parse(input):
     left, right = zip(*plint(input))
     return (sorted(left), sorted(right))
 
-def part_a(data):
-    left, right = data
+def part_a(left, right):
     return sum(abs(a-b) for a,b in zip(left, right))
 
-def part_b(data):
-    left, right = data
+def part_b(left, right):
     counts = Counter(right)
     return sum(n * counts[n] for n in left)
 
 puzzle = Puzzle(2024, 1)
 data = parse(puzzle.input_data)
-print("part A", part_a(data))
-print("part B", part_b(data))
+print("part A", part_a(*data))
+print("part B", part_b(*data))
