f4ebd833fc9347916a9740376ddf24339d1da0a3
garbidge <sleeplessghost@live.com>
2024-12-05 16:27:34 +1030

    simplify

--- 2024/05.py
+++ 2024/05.py
@@ -1,9 +1,10 @@
+from collections import defaultdict
 from aocd.models import Puzzle
 from utils import plint
 
 def parse(input):
     rules, pages = [plint(group) for group in input.split('\n\n')]
-    rules_grp = { left: set() for left,right in rules }
+    rules_grp = defaultdict(set)
     for left,right in rules: rules_grp[left].add(right)
     return (rules_grp, pages)
 
@@ -15,26 +16,17 @@ def part_b(rules_grp, pages):
     return sum(page[len(page) // 2] for page in invalid)
 
 def is_valid(page, rules_grp):
-    seen = set()
-    for p in page:
-        if p in rules_grp and any(r in seen for r in rules_grp[p]):
-            return False
-        seen.add(p)
-    return True
+    return not any(rule in page[:i] for i,p in enumerate(page) for rule in rules_grp[p])
 
 def reorder(page, rules_grp):
-    mutated = [*page]
-    index = len(page) - 1
-    while index >= 0:
-        current = mutated[index]
-        rules = rules_grp[current] if current in rules_grp else []
-        sliced = mutated[:index]
-        indices = [mutated.index(p) for p in rules if p in sliced]
+    mutated, index = [*page], len(page) - 1
+    while index > 0:
+        rules = rules_grp[mutated[index]]
+        indices = [mutated.index(p) for p in rules if p in mutated[:index]]
         if indices:
             value = mutated.pop(index)
             mutated.insert(min(indices), value)
-        else:
-            index -= 1
+        else: index -= 1
     return mutated
 
 puzzle = Puzzle(2024, 5)
