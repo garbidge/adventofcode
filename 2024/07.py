dfb8d319e63acd52c128dd539d604c5f1963e44d
garbidge <sleeplessghost@live.com>
2024-12-07 16:41:38 +1030

    recursive condition instead of generator

--- 2024/07.py
+++ 2024/07.py
@@ -7,23 +7,19 @@ def parse(input):
 
 def part_a(data):
     operations = (operator.add, operator.mul)
-    return sum(value for value,numbers in data if matches(value, numbers, operations))
+    return sum(value for value,numbers in data if is_match(value, 0, numbers, operations))
 
 def part_b(data):
     operations = (operator.add, operator.mul, lambda a,b: int(str(a) + str(b)))
-    return sum(value for value,numbers in data if matches(value, numbers, operations))
+    return sum(value for value,numbers in data if is_match(value, 0, numbers, operations))
 
-def matches(value, numbers, operations):
-    return any(value == p for p in possibles(value, 0, numbers, operations))
-
-def possibles(target, current, remaining, operations):
+def is_match(target, current, remaining, operations):
     if current > target:
-        return
+        return False
     if len(remaining) == 0:
-        yield current
-    else:
-        for new_value in (op(current,remaining[0]) for op in operations):
-            yield from possibles(target, new_value, remaining[1:], operations)
+        return target == current
+    next_values = (op(current,remaining[0]) for op in operations)
+    return any(is_match(target, new_value, remaining[1:], operations) for new_value in next_values)
 
 puzzle = Puzzle(2024, 7)
 data = parse(puzzle.input_data)
