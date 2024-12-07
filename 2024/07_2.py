7a59fc1406a2a21624545cc7f4c5d7488e11fe9c
garbidge <sleeplessghost@live.com>
2024-12-07 19:43:31 +1030

    simplify

--- 2024/07_2.py
+++ 2024/07_2.py
@@ -5,28 +5,20 @@ def parse(input):
     return [(n[0], tuple(n[1:])) for n in plint(input)]
 
 def part_a(data):
-    return sum(value for value,numbers in data if has_match(value, numbers, False))
+    return sum(value for value,numbers in data if any(rev_match(value, numbers, False)))
 
 def part_b(data):
-    return sum(value for value,numbers in data if has_match(value, numbers, True))
+    return sum(value for value,numbers in data if any(rev_match(value, numbers, True)))
 
-def has_match(target, remaining, with_concat):
-    return any(rev_match(target, target, remaining, with_concat))
-
-def rev_match(target, current, remaining, with_concat):
+def rev_match(current, remaining, with_concat):
     if current < 0: yield False
     elif len(remaining) == 0: yield current == 0
     else:
-        if with_concat:
-            yield from rev_concat(target, current, remaining[-1], remaining)
+        if with_concat and current % (divisor := pow(10, digits(remaining[-1]))) == remaining[-1]:
+            yield from rev_match(current // divisor, remaining[:-1], with_concat)
         if current % remaining[-1] == 0:
-            yield from rev_match(target, current // remaining[-1], remaining[:-1], with_concat)
-        yield from rev_match(target, current - remaining[-1], remaining[:-1], with_concat)
-
-def rev_concat(target, current, value, remaining):
-    if current % 10 == value % 10 and digits(current) > (length := digits(value)):
-        next_value = current // pow(10, length)
-        yield from rev_match(target, next_value, remaining[:-1], True)
+            yield from rev_match(current // remaining[-1], remaining[:-1], with_concat)
+        yield from rev_match(current - remaining[-1], remaining[:-1], with_concat)
 
 puzzle = Puzzle(2024, 7)
 data = parse(puzzle.input_data)
