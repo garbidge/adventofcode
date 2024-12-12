79442e3258fa84e6e8415b79c8ce3fbf8e9c8d29
garbidge <sleeplessghost@live.com>
2024-12-12 21:42:53 +1030

    tidy

--- 2024/12.py
+++ 2024/12.py
@@ -1,44 +1,37 @@
 from collections import deque
 from aocd.models import Puzzle
-from utils import coord_yield_dir, neighbrs_str8, pgriddict, tuple_add
+from utils import coord_dirs_str8, coord_yield_dir, neighbrs_str8, pgriddict, tuple_add
 
 def parse(input):
-    return pgriddict(input, str)
+    grid = pgriddict(input, str)
+    return find_regions(grid)
 
-def part_a(data):
-    regions = list()
-    for coord in data:
-        if not any(coord in region for region,_ in regions):
-            regions.append(find_region(data, coord))
+def part_a(regions):
     return sum(len(region) * perimeter for region,perimeter in regions)
 
-def part_b(data):
-    regions = list()
-    for coord in data:
-        if not any(coord in region for region,_ in regions):
-            regions.append(find_region(data, coord))
+def part_b(regions):
     total = 0
-    dirs = [
-        ((0,-1), (1,0), (-1,0)),
-        ((0,1), (1,0), (-1,0)),
-        ((1,0), (0,1), (0,-1)),
-        ((-1,0), (0,1), (0,-1))
-    ]
     for region,_ in regions:
         sides = 0
-        for dir, travel_a, travel_b in dirs:
+        for (dx,dy) in coord_dirs_str8(2):
             visited = set()
             for coord in region:
-                n = tuple_add(coord, dir)
+                n = tuple_add(coord, (dx,dy))
                 if n not in region and n not in visited:
                     visited.add(n)
                     sides += 1
-                    for same_side in coord_yield_dir(coord, travel_a, lambda c: c in region and tuple_add(c, dir) not in region):
-                        visited.add(tuple_add(same_side, dir))
-                    for same_side in coord_yield_dir(coord, travel_b, lambda c: c in region and tuple_add(c, dir) not in region):
-                        visited.add(tuple_add(same_side, dir))
+                    for parallel in ((dy,dx), (-dy,-dx)):
+                        for same_side in coord_yield_dir(coord, parallel, lambda c: c in region and tuple_add(c, (dx,dy)) not in region):
+                            visited.add(tuple_add(same_side, (dx,dy)))
         total += len(region) * sides
     return total
+
+def find_regions(data):
+    regions = list()
+    for coord in data:
+        if not any(coord in region for region,_ in regions):
+            regions.append(find_region(data, coord))
+    return regions
 
 def find_region(data, coord):
     points = set((coord,))
