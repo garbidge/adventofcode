5b0561b2ccf6aa68a1c5c925d7d57ccdb06c86ae
garbidge <sleeplessghost@live.com>
2024-12-17 10:22:02 +1030

    remove some pointless states

--- 2024/16.py
+++ 2024/16.py
@@ -2,59 +2,35 @@ from aocd.models import Puzzle
 from heapq import heappop, heappush
 from utils import pgriddict, tuple_add
 
-dirmap = [(1, 0), (0,-1), (-1,0), (0,1)]
+DIRECTIONS = [(1, 0), (0,-1), (-1,0), (0,1)]
 
 def parse(input):
     return pgriddict(input, str)
 
-def part_a(grid):
+def solve(grid):
     location = next(c for c in grid if grid[c] == 'S')
     h = []
-    visited = set()
-    heappush(h, (0, location, 0))
-    while h:
-        score, location, di = heappop(h)
-        if grid[location] == 'E':
-            return score
-        if (location,di) in visited:
-            continue
-        visited.add((location,di))
-        direction = dirmap[di]
-        nxt = tuple_add(location, direction)
-        if grid[nxt] != '#':
-            heappush(h, (score + 1, nxt, di))
-        heappush(h, (score + 1000, location, (di + 1) % len(dirmap)))
-        heappush(h, (score + 1000, location, (di - 1) % len(dirmap)))
-
-def part_b(grid):
-    location = next(c for c in grid if grid[c] == 'S')
-    h = []
-    visited_scores = dict()
-    best_tiles = set()
-    best_score = -1
+    visited_scores, best_tiles, best_score = dict(), set(), None
     heappush(h, (0, location, set([location]), 0))
     while h:
         score, location, path, di = heappop(h)
-        if best_score != -1 and score > best_score:
-            continue
+        if best_score and score > best_score:
+            break
         if grid[location] == 'E':
             best_tiles.update(path)
             best_score = score
             continue
-        if (location,di) in visited_scores:
-            if score > visited_scores[(location,di)]:
-                continue
-        if (location,di) not in visited_scores or visited_scores[(location,di)] > score:
-            visited_scores[(location,di)] = score
-        direction = dirmap[di]
-        nxt = tuple_add(location, direction)
-        if grid[nxt] != '#':
-            heappush(h, (score + 1, nxt, {*path, nxt}, di))
-        heappush(h, (score + 1000, location, path, (di + 1) % len(dirmap)))
-        heappush(h, (score + 1000, location, path, (di - 1) % len(dirmap)))
-    return len(best_tiles)
+        if (location,di) in visited_scores and score > visited_scores[(location,di)]:
+            continue
+        visited_scores[(location,di)] = score
+        for score_inc,direction_index in ((1, di), (1001, (di+1) % len(DIRECTIONS)), (1001, (di-1) % len(DIRECTIONS))):
+            direction = DIRECTIONS[direction_index]
+            if grid[(nxt := tuple_add(location, direction))] != '#':
+                heappush(h, (score + score_inc, nxt, {*path, nxt}, direction_index))
+    return best_score, len(best_tiles)
 
 puzzle = Puzzle(2024, 16)
 data = parse(puzzle.input_data)
-print("part A", part_a(data))
-print("part B", part_b(data))
+a,b = solve(data)
+print("part A", a)
+print("part B", b)
