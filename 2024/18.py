c7f32e916fa29c29857424a553f84898baf13aa7
garbidge <sleeplessghost@live.com>
2024-12-18 21:45:53 +1030

    tidy

--- 2024/18.py
+++ 2024/18.py
@@ -1,4 +1,4 @@
-from collections import defaultdict, deque
+from collections import deque
 from aocd.models import Puzzle
 from utils import neighbrs_str8, plint
 
@@ -6,57 +6,30 @@ def parse(input):
     return plint(input)
 
 def part_a(coords):
-    grid = get_grid(coords, 1024)
-    return len(find_path(grid))
+    grid = get_grid()
+    for x,y in coords[:1024]: grid[(x,y)] = '#'
+    return find_path(grid)
 
 def part_b(coords):
-    lower = 0
-    upper = len(coords) - 1
-    middle = (upper + lower) // 2
-    while middle > (lower + 1):
-        print(lower, middle, upper)
-        grid = get_grid(coords, middle)
-        path = find_path(grid)
-        if path is None:
-            upper = middle
-        else:
-            lower = middle
-        middle = (upper + lower) // 2
-    n = lower
-    grid = get_grid(coords, n)
-    path = find_path(grid)
-    print(n, ':', 'none' if path is None else len(path))
-    while path is not None:
-        n += 1
-        grid = get_grid(coords, n)
-        path = find_path(grid)
-        print(n, ':', 'none' if path is None else len(path), coords[n-1], coords[n], coords[n+1])
-    print(n, coords[n-1], coords[n], coords[n+1])
-    return coords[n-1]
+    grid = get_grid()
+    for x,y in coords: grid[(x,y)] = '#'
+    for x,y in reversed(coords):
+        grid[(x,y)] = '.'
+        if find_path(grid): return f'{x},{y}'
 
-def get_grid(coords, num):
-    grid = defaultdict(str)
-    for x in range(71):
-        for y in range(71):
-            grid[(x,y)] = '.'
-    for x,y in coords[:num]:
-        grid[(x,y)] = '#'
-    return grid
+def get_grid():
+    return {(x,y): '.' for y in range(71) for x in range(71)}
 
 def find_path(grid):
-    coord = (0,0)
-    target = (70,70)
-    q = deque([(coord,[])])
-    visited = set([coord])
+    q = deque([((0,0), 0)])
+    visited = set([(0,0)])
     while q:
-        coord, path = q.popleft()
-        if coord == target:
-            return path
+        coord, length = q.popleft()
+        if coord == (70,70): return length
         for n in neighbrs_str8(coord):
             if n in grid and n not in visited and grid[n] != '#':
                 visited.add(n)
-                q.append((n, [*path, n]))
-    return None
+                q.append((n, length + 1))
 
 puzzle = Puzzle(2024, 18)
 data = parse(puzzle.input_data)
