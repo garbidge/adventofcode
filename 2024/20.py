aeeef15073d0d8e543de3e1dceee3bbec1f8dc26
garbidge <sleeplessghost@live.com>
2024-12-21 02:51:02 +1030

    faster algorithm

--- 2024/20.py
+++ 2024/20.py
@@ -1,30 +1,35 @@
 from aocd.models import Puzzle
 from utils import neighbrs_str8, pgriddict
 
-def parse(input):
-    return pgriddict(input, str)
-
 def find_path(grid):
     coord = next(c for c in grid if grid[c] == 'S')
-    path, visited = [coord], set([coord])
+    distances, path = {coord: 0}, [coord]
     while grid[coord] != 'E':
-        coord = next(c for c in neighbrs_str8(coord) if c not in visited and grid[c] != '#')
+        coord = next(c for c in neighbrs_str8(coord) if c not in distances and grid[c] != '#')
+        distances[coord] = len(path)
         path.append(coord)
-        visited.add(coord)
-    return path
+    return distances, path
 
-def cheat(path, distance):
-    total = 0
-    for index_a,(x1,y1) in enumerate(path):
-        for index_b,(x2,y2) in enumerate(path):
-            if index_b > index_a:
-                point_dist = abs(y2-y1) + abs(x2-x1)
-                if point_dist <= distance and index_b - index_a - point_dist >= 100:
-                    total += 1
-    return total
+def cheat_paths(distances, path):
+    total_a, total_b = 0, 0
+    for n in path[:-1]:
+        for c,dist in coords_within_dist(*n, 20):
+            if c in distances:
+                time_saved = distances[c] - distances[n] - dist
+                if time_saved >= 100:
+                    if dist <= 2: total_a += 1
+                    total_b += 1
+    return total_a, total_b
+
+def coords_within_dist(x, y, distance):
+    for dx in range(-distance, distance + 1):
+        for dy in range(-distance, distance + 1):
+            if not (dx == 0 and dy == 0) and (dist := abs(dx) + abs(dy)) <= distance:
+                yield ((x + dx, y + dy), dist)
 
 puzzle = Puzzle(2024, 20)
-grid = parse(puzzle.input_data)
-path = find_path(grid)
-print("part A", cheat(path, 2))
-print("part B", cheat(path, 20))
+grid = pgriddict(puzzle.input_data, str)
+distances, path = find_path(grid)
+a, b = cheat_paths(distances, path)
+print(f'part A: {a}')
+print(f'part B: {b}')
