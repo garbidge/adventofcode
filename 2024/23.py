6ddf11749f138bd9de638c738aed6965023f5b32
garbidge <sleeplessghost@live.com>
2024-12-23 18:16:56 +1030

    simplify

--- 2024/23.py
+++ 2024/23.py
@@ -4,13 +4,13 @@ from aocd.models import Puzzle
 
 def parse(input):
     lookup = defaultdict(set)
-    for a, b in [line.split('-') for line in input.splitlines()]:
+    for a,b in (line.split('-') for line in input.splitlines()):
         lookup[a].add(b)
         lookup[b].add(a)
     return lookup
 
 def part_a(lookup):
-    interconnected = set(frozenset((a,b)) for a in lookup for b in lookup[a])
+    interconnected = {frozenset((a,b)) for a in lookup for b in lookup[a]}
     interconnected = add_neighbours(lookup, interconnected)
     return sum(any(computer.startswith('t') for computer in group) for group in interconnected)
 
@@ -21,12 +21,7 @@ def part_b(lookup):
     return ','.join(sorted(interconnected.pop()))
 
 def add_neighbours(lookup, interconnected):
-    result = set()
-    for group in interconnected:
-        intersection = reduce(set.intersection, (lookup[n] for n in group))
-        for n in intersection - group:
-            result.add(frozenset(group | {n}))
-    return result
+    return {group | {n} for group in interconnected for n in reduce(set.intersection, (lookup[c] for c in group)) - group}
 
 puzzle = Puzzle(2024, 23)
 data = parse(puzzle.input_data)
