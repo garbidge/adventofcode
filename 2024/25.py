05e115404809e5df3c9a300814bf6de09c384ab3
garbidge <sleeplessghost@live.com>
2024-12-25 15:51:27 +1030

    tidy

--- 2024/25.py
+++ 2024/25.py
@@ -1,24 +1,17 @@
 from aocd.models import Puzzle
 
 def parse(input):
-    return [parse_grid(x) for x in input.split('\n\n')]
+    locks, keys = [],[]
+    for group in input.split('\n\n'):
+        lines = group.splitlines()
+        sizes = [col.count('#') - 1 for col in zip(*lines)]
+        if all(c == '#' for c in lines[0]): locks.append(sizes)
+        else: keys.append(sizes)
+    return (locks,keys)
 
-def parse_grid(x):
-    lines = x.splitlines()
-    typ = 'lock' if all(c == '#' for c in lines[0]) else 'key'
-    cols = [[line[i] for line in lines] for i in range(len(lines[0]))]
-    return typ, [sum(c == '#' for c in col) - 1 for col in cols]
-
-def part_a(data):
-    locks = [grid for typ, grid in data if typ == 'lock']
-    keys = [grid for typ, grid in data if typ == 'key']
-    total = 0
-    for lock in locks:
-        for key in keys:
-            if all(a + b <= 5 for a,b in zip(lock,key)):
-                total += 1
-    return total
+def part_a(locks, keys):
+    return sum(all(a + b <= 5 for a,b in zip(lock,key)) for lock in locks for key in keys)
 
 puzzle = Puzzle(2024, 25)
-data = parse(puzzle.input_data)
-print("part A", part_a(data))
+locks,keys = parse(puzzle.input_data)
+print("part A", part_a(locks, keys))
