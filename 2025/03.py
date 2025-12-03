08740a84288e91a711e1306f9e8c426e3942a7f6
garbidge <sleeplessghost@live.com>
2025-12-04 00:29:26 +1030

    tidying

--- 2025/03.py
+++ 2025/03.py
@@ -3,30 +3,19 @@ from aocd.models import Puzzle
 def parse(input):
     return [tuple(int(n) for n in line) for line in input.splitlines()]
 
-def part_a(data):
-    return sum(bats(line, 2) for line in data)
-
-def part_b(data):
-    return sum(bats(line, 12) for line in data)
-
-def bats(line, digits):
-    index, result = 0, ''
-    for i in range(digits):
-        index = best_index(line, index, digits - i)
-        result += str(line[index])
+def batteries(line, digits):
+    index, result = 0, 0
+    for d in range(digits):
+        index = best_index(line, index, digits - d)
+        result = (10 * result) + line[index]
         index += 1
-    return int(result)
+    return result
 
 def best_index(line, start, remaining_digits):
-    best_index = start
-    for i in range(start + 1, len(line)):
-        if len(line) - i < remaining_digits:
-            break
-        if line[i] > line[best_index]:
-            best_index = i
-    return best_index
+    end = len(line) - remaining_digits
+    return max(range(start, end + 1), key=lambda i: line[i])
 
 puzzle = Puzzle(2025, 3)
 data = parse(puzzle.input_data)
-print("part A", part_a(data))
-print("part B", part_b(data))
+print("part A", sum(batteries(line, 2) for line in data))
+print("part B", sum(batteries(line, 12) for line in data))
