f0bd37c17ba3dfba04b668a161b408297192f393
garbidge <sleeplessghost@live.com>
2025-12-04 23:40:09 +1030

    update typing on utils

--- utils.py
+++ utils.py
@@ -43,7 +43,7 @@ from re import (  # noqa
     split,
     findall,
 )
-from typing import Callable, DefaultDict, Iterable, List, Any
+from typing import Callable, DefaultDict, Generator, Iterable, List, Any, Sequence
 
 # endregion
 
@@ -70,7 +70,7 @@ def pgrpints(input: str) -> List[List[List[int]]]:
 def pgridint(input: str) -> List[List[int]]:
     return [[int(n) for n in line] for line in input.splitlines()]
 
-def pgriddict(input: str, defaultFunc: Callable):
+def pgriddict(input: str, defaultFunc: Callable[[str], Any]) -> DefaultDict[tuple[int, int], Any]:
     ddict = defaultdict(defaultFunc)
     lines = input.splitlines()
     for row in range(len(lines)):
@@ -88,10 +88,10 @@ def preg(input: str, pattern: str) -> List[List[Any]]:
 def ints(line: str) -> List[int]:
     return lmap(int, re.findall(r"-?\d+", line))
 
-def lmap(func, collection: List[Any]) -> List:
+def lmap(func: Callable, collection: List[Any]) -> List:
     return list(map(func, collection))
 
-def lmapsub(func, collection: List[List[Any]]) -> List:
+def lmapsub(func: Callable, collection: List[List[Any]]) -> List:
     return lmap(lambda sub: [func(item) for item in sub], collection)
 
 def flatten(collection: List[List[Any]]) -> List[Any]:
@@ -103,7 +103,7 @@ def tuple_add(tuple_a: tuple, tuple_b: tuple) -> tuple:
 def tuple_sub(tuple_a: tuple, tuple_b: tuple) -> tuple:
     return tuple(a - b for a,b in zip(tuple_a, tuple_b))
 
-def print_ddict(ddict: DefaultDict[tuple, str]):
+def print_ddict(ddict: DefaultDict[tuple, str]) -> None:
     min_x, min_y = mins(ddict)
     max_x, max_y = maxes(ddict)
     for y in range(min_y, max_y + 1):
@@ -111,32 +111,32 @@ def print_ddict(ddict: DefaultDict[tuple, str]):
             print(ddict[(x, y)], end="")
         print()
 
-def transpose(iterable):
+def transpose(iterable: Iterable[Sequence[Any]]) -> List[List[Any]]:
     return lmap(list, zip(*iterable))
 
-def transpose_str(collection: List[str]):
+def transpose_str(collection: List[str]) -> List[str]:
     return [''.join(collection[r][c] for r in range(len(collection))) for c in range(len(collection[0]))]
 
-def maxes(iterable: Iterable[tuple]):
+def maxes(iterable: Iterable[tuple]) -> tuple:
     listified = [*iterable]
     return tuple(max(tup[i] for tup in listified) for i in range(len(listified[0])))
 
-def mins(iterable: Iterable[tuple]):
+def mins(iterable: Iterable[tuple]) -> tuple:
     listified = [*iterable]
     return tuple(min(tup[i] for tup in listified) for i in range(len(listified[0])))
 
-def chunks(collection: List, size: int):
+def chunks(collection: Sequence[Any], size: int) -> Generator[Sequence[Any], None, None]:
     for i in range(0, len(collection), size):
         yield collection[i:i+size]
 
-def digits(number: int):
+def digits(number: int) -> int:
     s = str(number)
     return len(s) - (1 if number < 0 else 0)
 
-def sign(x: int):
+def sign(x: int) -> int:
     return (x > 0) - (x < 0)
 
-def is_prime(n):
+def is_prime(n: int) -> bool:
     if n <= 1:
         return False
     for i in range(2, int(math.sqrt(n)) + 1):
@@ -148,30 +148,30 @@ def is_prime(n):
 
 # region points and grids
 
-def coord_dirs(dimensions: int):
+def coord_dirs(dimensions: int) -> List[tuple]:
     return list(itertools.product((-1, 0, 1), repeat=dimensions))
 
-def coord_dirs_diag(dimensions: int):
+def coord_dirs_diag(dimensions: int) -> List[tuple]:
     return list(itertools.product((-1, 1), repeat=dimensions))
 
-def coord_dirs_str8(dimensions: int):
+def coord_dirs_str8(dimensions: int) -> List[tuple]:
     return list(
         d
         for d in itertools.product((-1, 0, 1), repeat=dimensions)
         if d.count(0) == dimensions - 1
     )
 
-def neighbrs(coordinate: tuple) -> tuple:
+def neighbrs(coordinate: tuple) -> List[tuple]:
     zeros = tuple(itertools.repeat(0, len(coordinate)))
     return [tuple_add(coordinate, d) for d in coord_dirs(len(coordinate)) if d != zeros]
 
-def neighbrs_diag(coordinate: tuple) -> tuple:
+def neighbrs_diag(coordinate: tuple) -> List[tuple]:
     return [tuple_add(coordinate, d) for d in coord_dirs_diag(len(coordinate))]
 
-def neighbrs_str8(coordinate: tuple) -> tuple:
+def neighbrs_str8(coordinate: tuple) -> List[tuple]:
     return [tuple_add(coordinate, d) for d in coord_dirs_str8(len(coordinate))]
 
-def coord_yield_dir(coord: tuple, direction: tuple, condition: Callable[[tuple], bool] = lambda _: True):
+def coord_yield_dir(coord: tuple, direction: tuple, condition: Callable[[tuple], bool] = lambda _: True) -> Generator[tuple, None, None]:
     coord = tuple_add(coord, direction)
     while condition(coord):
         yield coord
