c9883b0afe577f5201e7f3b34d91831cb5d94c68
garbidge <sleeplessghost@live.com>
2025-12-06 16:56:05 +1030

    more tidying

--- 2025/06.py
+++ 2025/06.py
@@ -5,17 +5,15 @@ from aocd.models import Puzzle
 from utils import transpose_str
 
 def solve(input):
-    ops = {'+': sum, '*': math.prod}
+    ops = { '+': sum, '*': math.prod }
     *numbers, operators = input.splitlines()
     numbers, operators =  transpose_str(numbers), operators.split()
-    numbers = [list(group) for key,group in itertools.groupby(numbers, lambda x: x.strip() == '') if not key]
+    groupings = [list(group) for key,group in itertools.groupby(numbers, lambda x: x.strip() == '') if not key]
     a = b = 0
-    for i,nums in enumerate(numbers):
-        vertical = [int(n) for n in nums]
-        horizontal = [int(n) for n in transpose_str(nums)]
+    for i,number_group in enumerate(groupings):
         operator = ops[operators[i]]
-        a += operator(horizontal)
-        b += operator(vertical)
+        a += operator(int(n) for n in transpose_str(number_group))
+        b += operator(int(n) for n in number_group)
     return a, b
 
 puzzle = Puzzle(2025, 6)
