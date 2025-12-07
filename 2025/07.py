e441c5300dbd448e58dc5d7491eb045fe4cb48cd
garbidge <sleeplessghost@live.com>
2025-12-07 17:25:42 +1030

    tidy more

--- 2025/07.py
+++ 2025/07.py
@@ -1,27 +1,22 @@
 from collections import Counter
 from aocd.models import Puzzle
-from utils import pgriddict
 
-def parse(input):
-    return pgriddict(input, str)
-
-def solve(data):
+def solve(input):
+    grid = input.splitlines()
     splitters = set()
-    counts = Counter({next(c for c in data if data[c] == "S"): 1})
-    max_y = 0
-    for x,y in sorted(data, key=lambda xy: xy[1]):
-        max_y = y
-        if (x, y - 1) in counts:
-            if data[(x, y)] == '^':
-                splitters.add((x, y))
-                counts[(x - 1, y)] += counts[(x, y - 1)]
-                counts[(x + 1, y)] += counts[(x, y - 1)]
-            else:
-                counts[(x, y)] += counts[(x, y - 1)]
-    return len(splitters), sum(count for (x,y),count in counts.items() if y == max_y)
+    counts = Counter({(grid[0].index('S'), 0): 1})
+    for y,line in enumerate(grid):
+        for x,char in enumerate(line):
+            if (x, y - 1) in counts:
+                if char == '^':
+                    splitters.add((x, y))
+                    counts[(x - 1, y)] += counts[(x, y - 1)]
+                    counts[(x + 1, y)] += counts[(x, y - 1)]
+                else:
+                    counts[(x, y)] += counts[(x, y - 1)]
+    return len(splitters), sum(count for (x,y),count in counts.items() if y == len(grid) - 1)
 
 puzzle = Puzzle(2025, 7)
-data = parse(puzzle.input_data)
-a,b = solve(data)
+a,b = solve(puzzle.input_data)
 print("part A", a)
 print("part B", b)
