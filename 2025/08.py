0ea44e9518ee490b7ac942d6baec7d6eacedee75
garbidge <sleeplessghost@live.com>
2025-12-08 23:02:22 +1030

    tidy more

--- 2025/08.py
+++ 2025/08.py
@@ -6,27 +6,20 @@ def parse(input):
     return [tuple(int(n) for n in line.split(",")) for line in input.splitlines()]
 
 def solve(data):
-    a, b = 0, 0
     ordered = sorted(itertools.combinations(data, 2), key=lambda pair: math.dist(*pair))
-    circuits = [set([pos]) for pos in data]
-    for count,(i,j) in enumerate(ordered):
-        if count == 1000:
-            a = math.prod(sorted([len(x) for x in circuits], reverse=True)[:3])
-        combine_sets(circuits, i, j)
-        if len(circuits) == 1:
-            break
-    (x1,y1,z1) = i
-    (x2,y2,z2) = j
-    b = x1 * x2
-    return a,b
+    i, circuits = 0, [{pos} for pos in data]
+    while len(circuits) > 1:
+        if i == 1000:
+            yield math.prod(sorted((len(x) for x in circuits), reverse=True)[:3])
 
-def combine_sets(circuits, pos_a, pos_b):
-    set_a = next(s for s in circuits if pos_a in s)
-    set_b = next(s for s in circuits if pos_b in s)
-    if set_a != set_b:
-        circuits.remove(set_a)
-        circuits.remove(set_b)
-        circuits.append(set_a.union(set_b))
+        pos_a, pos_b = ordered[i]
+        i += 1
+        sets = [s for s in circuits if pos_a in s or pos_b in s]
+        if len(sets) == 2:
+            sets[0] |= sets[1]
+            circuits.remove(sets[1])
+
+    yield pos_a[0] * pos_b[0]
 
 puzzle = Puzzle(2025, 8)
 data = parse(puzzle.input_data)
